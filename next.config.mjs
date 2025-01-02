fbada93fcd6c79b79df1842314a4bb1dc70445c0
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-02 15:59:57 +0000

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: "raw-loader",
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/cron", // Apply only to the /api/cron path
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store", // Prevent caching
+          },
+          {
+            key: "Pragma",
+            value: "no-cache", // Prevent caching (legacy support)
+          },
+          {
+            key: "Expires",
+            value: "0", // Expire immediately
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
