0b411b5f999862beedd31896af2622ff443aa397
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-02 22:14:50 +0000

    remove headers

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -2,15 +2,7 @@ import { NextResponse } from "next/server";
 import supabase from "@/services/supabase/setup";
 
 export const revalidate = 0;
-// export const dynamic = 'force-static'
 export async function GET() {
-  // Set headers to disable caching
-  const responseHeaders = {
-    "Cache-Control": "public, s-maxage=1",
-    "CDN-Cache-Control": "public, s-maxage=1",
-    "Vercel-CDN-Cache-Control": "public, s-maxage=1",
-  };
-
   console.log("🚀 ~ Cron endpoint triggered in production...");
 
   const { error } = await supabase
--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,27 +1,9 @@
 import { NextResponse } from "next/server";
 
-// export const fetchCache = "force-no-store";
-// export async function GET() {
-
-//   return NextResponse.json(
-//     {
-//       success: true,
-//       message: "Testing API route",
-//       timestamp: new Date().toISOString(),
-//     },
-//     { status: 200 }
-//   );
-// }
-// export const revalidate = 0;
-export const dynamic = 'force-static'
+export const revalidate = 0;
 export async function GET() {
-  console.log("🚀 ~ GET ~ GET:TESSTTTT")
-  return new Response("Cache Control example", {
+  console.log("🚀 ~ GET ~ GET:TESSTTTT");
+  return new NextResponse("Cache Control example", {
     status: 200,
-    headers: {
-      "Cache-Control": "public, s-maxage=0",
-      "CDN-Cache-Control": "public, s-maxage=0",
-      "Vercel-CDN-Cache-Control": "public, s-maxage=0",
-    },
   });
 }
