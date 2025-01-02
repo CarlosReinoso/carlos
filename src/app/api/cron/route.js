92272be8d7c647fa44b60670fb62224f8382bb73
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-02 15:26:58 +0000

    with cron-job

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,29 +1,17 @@
 import { NextResponse } from "next/server";
 import supabase from "@/services/supabase/setup";
-import cron from "node-cron";
-
-let taskScheduled = false;
 
 export async function GET() {
-  if (!taskScheduled) {
-    // cron.schedule("0 0 9 * * *", async () => {
-    cron.schedule("*/10 * * * * *", async () => {
-      console.log("🚀 ~ Scheduled task running...");
+  console.log("🚀 ~ Keep-alive API triggered...");
 
-      const { error } = await supabase
-        .from("supabase_alive")
-        .insert([{ created_at: new Date().toISOString().split("T")[0] }]);
+  const { error } = await supabase
+    .from("supabase_alive")
+    .insert([{ created_at: new Date().toISOString().split("T")[0] }]);
 
-      if (error) {
-        console.error("Error logging keep-alive action:", error);
-      } else {
-        console.log("Successfully logged keep-alive action.");
-      }
-    });
-
-    taskScheduled = true; // Prevent multiple schedules
-    console.log("Cron job scheduled.");
+  if (error) {
+    console.error("Error logging keep-alive action:", error);
+    return NextResponse.json({ success: false, error });
   }
 
-  return NextResponse.json({ message: "Cron job initialized." });
+  return NextResponse.json({ success: true, message: "Keep-alive action logged." });
 }
