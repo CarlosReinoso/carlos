856274306d334f18016669115d5ff47d74532b95
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-02 21:52:18 +0000

    max=0

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -17,9 +17,9 @@ export async function GET() {
   return new Response("Cache Control example", {
     status: 200,
     headers: {
-      "Cache-Control": "public, s-maxage=1",
-      "CDN-Cache-Control": "public, s-maxage=60",
-      "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+      "Cache-Control": "public, s-maxage=0",
+      "CDN-Cache-Control": "public, s-maxage=0",
+      "Vercel-CDN-Cache-Control": "public, s-maxage=0",
     },
   });
 }
