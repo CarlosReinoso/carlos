ca2a6c4b5baf9604fdc549579daa9089259e430c
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-02 21:44:17 +0000

    cahe testing

--- next.config.mjs
+++ next.config.mjs
@@ -7,28 +7,6 @@ const nextConfig = {
     });
     return config;
   },
-
-  async headers() {
-    return [
-      {
-        source: "/api/cron", // Apply only to the /api/cron path
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store", // Prevent caching
-          },
-          {
-            key: "Pragma",
-            value: "no-cache", // Prevent caching (legacy support)
-          },
-          {
-            key: "Expires",
-            value: "0", // Expire immediately
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
