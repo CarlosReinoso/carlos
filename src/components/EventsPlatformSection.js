2fca4f9881bf3ab34c79aa2acde39e2c37c1e0f2
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-01-04 14:34:11 +0000

    fix events platform section responsiveness

--- src/components/EventsPlatformSection.js
+++ src/components/EventsPlatformSection.js
@@ -1,20 +1,23 @@
 import Typography from "./common/Typography";
 import SVGWave from "./common/SVGWave";
 import Button from "./common/Button";
+import Link from "next/link";
 
 const EventsPlatformSection = () => {
   return (
     <div>
-      <div className="relative h-[50vh] bg-[url('/homepage/pink-moon.jpg')] bg-cover bg-center bg-black bg-opacity-50 bg-blend-overlay text-center">
+      <div className="relative h-[60vh] sm:h-[60vh] md:h-[60vh] lg:h-[80vh] 2xl:h-[100vh] bg-[url('/homepage/pink-moon.jpg')] bg-cover bg-center bg-black bg-opacity-50 bg-blend-overlay text-center">
         <SVGWave style={{ transform: "scale(1, -1) translateY(0.2px)" }} />
-        <div className="flex flex-col justify-around h-[25vh]">
+        <div className="flex flex-col justify-around h-[30vh] sm:h-[30vh] md:h-[30vh]">
           <Typography variant="h2" className="font-monoton text-shadow ">
             Events Near You
           </Typography>
           <Typography variant="h2" className="font-emblema text-white">
             Dance With Our Partners
           </Typography>
-          <Button theme="dark">Find Events</Button>
+          <Link href={"/events"}>
+            <Button theme="dark">Find Events</Button>
+          </Link>
         </div>
       </div>
     </div>
