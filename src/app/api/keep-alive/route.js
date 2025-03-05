8f77f564f133a7bbd810ad9f28ca3324119f4301
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-03-05 13:38:56 +0000

    pause supabase setup

--- src/app/api/keep-alive/route.js
+++ src/app/api/keep-alive/route.js
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server";
-import supabase from "@/services/supabase/config";
+// import supabase from "@/services/supabase/config";
 
 export const revalidate = 0;
 export async function GET() {
   console.log("🚀 ~ Cron endpoint triggered in production...");
 
-  const { error } = await supabase
-    .from("supabase_alive")
-    .insert([{ created_at: new Date().toISOString().split("T")[0] }]);
+  // const { error } = await supabase
+  //   .from("supabase_alive")
+  //   .insert([{ created_at: new Date().toISOString().split("T")[0] }]);
 
-  if (error) {
-    console.error("Error logging keep-alive action:", error);
-    return NextResponse.json({ success: false, error }, { status: 500 });
-  }
+  // if (error) {
+  //   console.error("Error logging keep-alive action:", error);
+  //   return NextResponse.json({ success: false, error }, { status: 500 });
+  // }
 
   return NextResponse.json(
     { success: true, message: "Keep-alive action logged." },
