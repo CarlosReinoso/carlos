202f2ebba3b400fde0ad4458810678e8f69ddd87
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-03-11 12:58:34 +0000

    web dev meta

--- src/metadata/web-dev.js
+++ src/metadata/web-dev.js
@@ -1,9 +1,12 @@
 const title =
   "Carlos Reinoso | Web Development | Helping solo entrepreneurs and small businesses create stunning websites that drive success";
+
+const description =
+  "I design and develop digital experiences tailored for solo entrepreneurs and small businesses. With experience working in startups to large FTS100 companies. I now focus on empowering independent business owners—artists, property investors, Airbnb hosts as well as psychosomatic teachers and many more—to establish a strong online presence. Whether it's a sleek portfolio, a booking platform, or an eCommerce site, I create websites that are intuitive, visually compelling, and built for growth.To relax I like to play guitar and sing, stay active at the gym, and love exploring new places. Right now, I’m planning an epic world trip to inspire my creativity for music, poetry and philosophy.";
+
 export const metadataWebDev = {
   title,
-  description:
-    "I design and develop digital experiences tailored for solo entrepreneurs and small businesses. With experience working in startups to large FTS100 companies. I now focus on empowering independent business owners—artists, property investors, Airbnb hosts as well as psychosomatic teachers and many more—to establish a strong online presence. Whether it's a sleek portfolio, a booking platform, or an eCommerce site, I create websites that are intuitive, visually compelling, and built for growth.To relax I like to play guitar and sing, stay active at the gym, and love exploring new places. Right now, I’m planning an epic world trip to inspire my creativity for music, poetry and philosophy.",
+  description,
   keywords: [
     "Web Development",
     "Next.js Developer",
@@ -23,16 +26,14 @@ export const metadataWebDev = {
   },
   openGraph: {
     title,
-    description:
-      "Explore the latest in web development, including React, Next.js, Tailwind CSS, and SQL. Learn full-stack techniques and best practices.",
-    url: "https://carlosreinoso.co.uk/web-dev",
-    site_name: "Carlos Reinoso",
+    description,
+    site_name: title,
     images: [
       {
         url: "/favicons/web-dev.ico",
         width: 1200,
         height: 630,
-        alt: "Carlos Reinoso Web Development - Next.js, Tailwind, SQL",
+        alt: title,
       },
     ],
     type: "website",
