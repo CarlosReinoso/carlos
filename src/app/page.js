1f22376655885250a1306d80b5ed2b176fb133ed
Carlos Reinoso <jrp.carlos@hotmail.com>
2025-03-22 15:43:10 +0000

    kingdom home builders

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ export default function LandingPage() {
   return (
     <>
       <nav className="flex flex-col h-full space-y-1 items-center justify-center">
-        <Link href="/web-dev">web dev</Link>
+        {/* <Link href="/web-dev">web dev</Link> */}
         <Link href="/property">property</Link>
-        <Link href="/music">music</Link>
+        {/* <Link href="/music">music</Link> */}
       </nav>
     </>
   );
